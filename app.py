b35f7a550d9f1d82c346396c5f3805ea06bac40d
User <happy832674@gmail.com>
2023-01-16 13:17:04 +0900

    2023-01-16

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import pandas as pd
+
 st.title('수학의 세계')
 st.header('정수의 덧셈과 뺄셈')
 st.success('유리수의 덧셈과 뺄셈')
+
+st.write("Hello, *World!* :sunglasses:")
+st.write("1234")
+st.write(1234)
+
+st.write(pd.DataFrame({
+    "first column":[1,2,3,4], "second column":[10,20,30,40],
+}))
+
